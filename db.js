ad0ca7569bf16ba60ae61072be4b4671e191cf0f
AmaMusicAcademy <serviceamamusicacademy@gmail.com>
2025-06-08 12:43:42 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: sql.amamusicacademy.it,       // es. 'localhost' o indirizzo DB Tophost
-  user: amamusic47740,        // utente DB
-  password: amam45804,  // password DB
-  database: amamusic47740,    // nome DB
+  host: 'sql.amamusicacademy.it',       // es. 'localhost' o indirizzo DB Tophost
+  user: 'amamusic47740',        // utente DB
+  password: 'amam45804',  // password DB
+  database: 'amamusic47740',    // nome DB
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
