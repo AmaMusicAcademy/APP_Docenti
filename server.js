76bd4d04312dda695f91ff30793df8b983346e5b
AmaMusicAcademy <serviceamamusicacademy@gmail.com>
2025-06-08 12:35:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const pool = require('./db');
+
 app.use(express.json());
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'API funzionante!' });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server in ascolto sulla porta ${PORT}`);
-});
-
 
 // rotta per testare la connessione a MySQL
 app.get('/api/dbtest', async (req, res) => {
@@ -26,3 +23,4 @@ app.get('/api/dbtest', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server in ascolto sulla porta ${PORT}`);
 });
+
