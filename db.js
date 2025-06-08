97e43b9d0fea754c92fa4105ae59361ee4d759f6
AmaMusicAcademy <serviceamamusicacademy@gmail.com>
2025-06-08 14:12:57 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  host: 'dpg-d12n0s49c44c738g0drg-a',
-  port: 5432,
-  user: 'accademia_db_user',
-  password: 't4VtyqmwjujGaAhiyy0nn3GB5g6ipVKf',
-  database: 'accademia_db',
-  ssl: {
-    rejectUnauthorized: false,
-  }
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: { rejectUnauthorized: false }
 });
 
-module.exports = pool;
+module.exports = { pool };
